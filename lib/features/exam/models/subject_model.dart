5af937b3db8560cf653a3646ab49ea09561044a0
Abrham <gmikael1908@gmail.com>
2026-04-09 15:46:53 +0300

    define subject modele

---
+++ lib/features/exam/models/subject_model.dart
@@ -0,0 +1,43 @@
+class SubjectMoModel {
+  int? id;
+  String name;
+  bool isNatural;
+  bool isCommon;
+
+  SubjectMoModel({
+    this.id,
+    required this.name,
+    required this.isNatural,
+    this.isCommon = false,
+  });
+
+  // Convert Json to SubjectMoModel object
+  factory SubjectMoModel.fromJson(Map<String, dynamic> json) {
+    return SubjectMoModel(
+      id: json['id'],
+      name: json['name'],
+      isNatural: json['is_natural'],
+      isCommon: json['is_common'],
+    );
+  }
+
+  // Convert DB Map to SubjectMoModel object
+  factory SubjectMoModel.fromMap(Map<String, dynamic> map) {
+    return SubjectMoModel(
+      id: map['id'],
+      name: map['name'],
+      isNatural: map['is_natural'] == 1,
+      isCommon: map['is_common'] == 1,
+    );
+  }
+
+  // Convert SubjectMoModel object to Map for DB insertion
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'is_natural': isNatural ? 1 : 0,
+      'is_common': isCommon ? 1 : 0,
+    };
+  }
+}
